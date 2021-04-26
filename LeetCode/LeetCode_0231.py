b74b5c4c9f0185bfe4b1234410762080984182df
sucpark <sucpark0317@gmail.com>
2021-04-26 22:37:02 +0900

    Update the problem

--- LeetCode/LeetCode_0231.py
+++ LeetCode/LeetCode_0231.py
@@ -3,8 +3,9 @@
 
 1) Check the edge cases
 2) Check the remainder by 2
-"""
 
+3) Use bitwise operations
+"""
 
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
@@ -12,16 +13,25 @@ class Solution:
         if n <= 0:
             return False
 
-        if n == 1:
-            return True
-
-        remainder = 0
-
-        while remainder == 0 and n > 2:
-            remainder = n % 2
-            n = n // 2
-
-        if remainder == 0:
+        if (n & n-1) == 0:
             return True
         else:
             return False
+
+#         if n <= 0:
+#             return False
+
+#         if n == 1:
+#             return True
+
+#         remainder = 0
+
+#         while remainder == 0 and n > 2:
+
+#             remainder = n % 2
+#             n = n // 2
+
+#         if remainder == 0:
+#             return True
+#         else:
+#             return False
