f630e5142fd2a9a3ea1a2ae801851e6deea8fac8
sucpark <sucpark0317@gmail.com>
2021-04-26 22:32:03 +0900

    Solve a new probelm

---
+++ LeetCode/LeetCode_0231.py
@@ -0,0 +1,27 @@
+"""
+231. Power of Two (easy)
+
+1) Check the edge cases
+2) Check the remainder by 2
+"""
+
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+
+        if n <= 0:
+            return False
+
+        if n == 1:
+            return True
+
+        remainder = 0
+
+        while remainder == 0 and n > 2:
+            remainder = n % 2
+            n = n // 2
+
+        if remainder == 0:
+            return True
+        else:
+            return False
