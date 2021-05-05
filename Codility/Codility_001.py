460f57c5a30c2bad97e04f92b8a897621128348d
sucpark <sucpark0317@gmail.com>
2021-05-05 15:03:07 +0900

    Solve a new problem

---
+++ Codility/Codility_001.py
@@ -0,0 +1,23 @@
+"""
+1. BinaryGap
+Find longest sequence of zeros in binary representation of an integer.
+
+"""
+
+def solution(N):
+    # write your code in Python 3.6
+
+    max_len = 0
+    temp_len = -1
+
+    while N != 0:
+        b = N % 2
+        N = N // 2
+
+        if b == 1:
+            max_len = max(max_len, temp_len)
+            temp_len = 0
+        elif b == 0 and temp_len >= 0:
+            temp_len += 1
+
+    return max_len
