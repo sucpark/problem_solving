a1f9b9ebf926778c6f69e939e8c6a4f62a55c696
sucpark <sucpark0317@gmail.com>
2021-05-09 23:39:58 +0900

    Solve a new problem

---
+++ LeetCode/LeetCode_0122.py
@@ -0,0 +1,13 @@
+"""
+122. Best Time to Buy and Sell Stock II (easy)
+
+
+"""
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        result = 0
+        for i in range(len(prices)-1):
+            if prices[i+1] > prices[i]:
+                result += prices[i+1] - prices[i]
+        return result
