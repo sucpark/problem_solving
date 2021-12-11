a7293bb429a7eec732210ae5f2a96ff6d15ea01a
sucpark <sucpark0317@gmail.com>
2021-12-11 20:43:52 +0900

    Solve the new problem

--- LeetCode/LeetCode_1295.py
+++ LeetCode/LeetCode_1295.py
@@ -14,3 +14,26 @@ class Solution:
             if len(str(x)) % 2 == 0:
                 cnt += 1
         return cnt
+
+
+# def CheckEvenDigit(num: int) -> bool:
+#     cnt = 0
+#     while num > 0:
+#         num //= 10
+#         cnt += 1
+#
+#     if cnt % 2 == 0:
+#         return True
+#     else:
+#         return False
+#
+#
+# class Solution:
+#     def findNumbers(self, nums: List[int]) -> int:
+#
+#         cnt = 0
+#         for x in nums:
+#             if CheckEvenDigit(x):
+#                 cnt += 1
+#
+#         return cnt
