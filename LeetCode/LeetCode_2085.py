6755aa319f0fa62133965479a5cfebffe8b8eb74
sucpark <sucpark0317@gmail.com>
2021-12-25 22:21:47 +0900

    Solve the new problem

---
+++ LeetCode/LeetCode_2085.py
@@ -0,0 +1,15 @@
+"""
+2085. Count Common Words With One Occurrence (Easy)
+
+"""
+
+
+class Solution:
+    def countWords(self, words1: List[str], words2: List[str]) -> int:
+
+        sol = 0
+        for w in words1:
+            if words1.count(w) == 1 and words2.count(w) == 1:
+                sol += 1
+
+        return sol
