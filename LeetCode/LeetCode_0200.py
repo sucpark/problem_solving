5525897fdb9f22ed11a26f472b0e10979d8b6345
sucpark <sucpark0317@gmail.com>
2022-01-09 23:03:37 +0900

    Solve the new problem

--- LeetCode/LeetCode_0200.py
+++ LeetCode/LeetCode_0200.py
@@ -1,35 +1,58 @@
 """
-200. Number of Islands (medium)
+200. Number of Islands (Medium)
 
-1) use dfs
-2) when I use bfs (use deque() and q.popleft()), time limit exceed
+Description
+DFS problem. Recursive or Stack
 
 """
 
 
-# from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
-        r,c = len(grid), len(grid[0])
-        q = [] # deque()
-        sol = 0
+        if not grid:
+            return 0
 
-        visited = [[0]*c for _ in range(r)]
+        def dfs(i, j):
 
-        for i in range(r):
-            for j in range(c):
-                if grid[i][j] == '1' and visited[i][j] == 0:
-                    q.append((i,j))
-                    while q:
-                        x, y = q.pop()  # q.popleft()
-                        visited[x][y] = 1
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+                return
 
-                        for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                            nx, ny = x+dx, y+dy
+            grid[i][j] = "0"
+            dfs(i + 1, j)  # North
+            dfs(i, j + 1)  # East
+            dfs(i - 1, j)  # South
+            dfs(i, j - 1)  # East
 
-                            if (0 <= nx < r) and (0 <= ny < c) and (grid[nx][ny] == '1') and (visited[nx][ny] == 0):
-                                q.append((nx, ny))
-                    sol += 1
-        return sol
+        cnt = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    cnt += 1
+
+        return cnt
+
+
+# class Solution:
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#
+#         r, c = len(grid), len(grid[0])
+#         visited = [[False for _ in range(c)] for _ in range(r)]
+#         island = 0
+#
+#         for i in range(r):
+#             for j in range(c):
+#                 if grid[i][j] == "1" and not visited[i][j]:
+#                     stack = [(i, j)]
+#                     while stack:
+#                         x, y = stack.pop()
+#                         visited[x][y] = True
+#                         for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+#                             nx, ny = x + dx, y + dy
+#                             if (0 <= nx < r) and (0 <= ny < c) and grid[nx][ny] == "1" and not visited[nx][ny]:
+#                                 stack.append((nx, ny))
+#                     island += 1
+#         return island
 
