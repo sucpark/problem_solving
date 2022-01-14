34426c7a395ef2baf651386df75c913a391adc0b
sucpark <sucpark0317@gmail.com>
2022-01-14 23:38:50 +0900

    Solve the new problem

--- LeetCode/LeetCode_0112.py
+++ LeetCode/LeetCode_0112.py
@@ -1,6 +1,9 @@
 """
 112. Path Sum (Easy)
 
+1) DFS
+2) Stack
+
 """
 
 
@@ -11,21 +14,43 @@
 #         self.left = left
 #         self.right = right
 
-def preorder(node, val, target):
-    if node:
-        val += node.val
-
-        if node.left or node.right:
-            return preorder(node.left, val, target) or preorder(node.right, val, target)
-        else:
-            if val == target:
-                return True
-            else:
-                return False
-
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+
         if not root:
             return False
 
-        return preorder(root, 0, targetSum)
+        stack = [(root, 0)]
+
+        while stack:
+            node, curr = stack.pop()
+            curr += node.val
+            if node.left:
+                stack.append((node.left, curr))
+            if node.right:
+                stack.append((node.right, curr))
+
+            if not node.left and not node.right:
+                if curr == targetSum:
+                    return True
+
+        return False
+
+# def preorder(node, val, target):
+#     if node:
+#         val += node.val
+#
+#         if node.left or node.right:
+#             return preorder(node.left, val, target) or preorder(node.right, val, target)
+#         else:
+#             if val == target:
+#                 return True
+#             else:
+#                 return False
+#
+# class Solution:
+#     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+#         if not root:
+#             return False
+#
+#         return preorder(root, 0, targetSum)
