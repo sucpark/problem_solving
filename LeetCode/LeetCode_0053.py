d9f7e32463a2d3a3858a03dafdd4817f835b1dea
sucpark <sucpark0317@gmail.com>
2022-01-27 21:46:33 +0900

    Solve the new problem

--- LeetCode/LeetCode_0053.py
+++ LeetCode/LeetCode_0053.py
@@ -9,24 +9,24 @@ Description
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
 
-        sol, summ = -sys.maxsize, 0
+        # sol, summ = -sys.maxsize, 0
+        #
+        # for n in nums:
+        #     if summ > 0:
+        #         summ += n
+        #     else:
+        #         summ = n
+        #
+        #     if sol < summ:
+        #         sol = summ
+        #
+        # return sol
 
-        for n in nums:
-            if summ > 0:
-                summ += n
-            else:
-                summ = n
+        currSum = maxSum = nums[0]
 
-            if sol < summ:
-                sol = summ
+        for i in range(1, len(nums)):
 
-        return sol
+            currSum = max(nums[i], currSum+nums[i])
+            maxSum = max(currSum, maxSum)
 
-#         currSum = maxSum = nums[0]
-
-#         for i in range(1, len(nums)):
-
-#             currSum = max(nums[i], currSum+nums[i])
-#             maxSum = max(currSum, maxSum)
-
-#         return maxSum
+        return maxSum
